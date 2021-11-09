64d97c96fd3fca84a0e668e52f503f1b9d4cd8f8
Odetyte <odetamaslovaite@yahoo.com>
2021-11-09 14:08:50 +0100

    create views for recipes controller action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # set routes to access view
+  root to: 'recipes#home'
+    get "/:id", to: "recipes#show", as: :recipe
 end
